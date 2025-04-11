56f0eb4da7a00ee2967e559750c282bfc2ae31c6
Houdaifa Bouamine <houdaifa.bouamine@gmail.com>
2025-04-11 16:16:55 +0100

    fix CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: ['http://localhost:5173'], // change this in production to the client url
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  });
+  
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3001);
   
 }
