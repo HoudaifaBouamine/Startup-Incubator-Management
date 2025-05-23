a4d0eb6a21405a5e5845fca785bf1adcf2caeb0a
Djamal DIB <d.dib@esi-sba.dz>
2025-05-23 18:09:39 +0100

    fix yarn

--- client/src/pages/Home.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Link } from "react-router-dom";
-const Home = () => {
-    return (_jsxs("div", { children: [_jsx("h1", { className: " text-3xl", children: "Welcome" }), _jsx(Link, { to: '/login', children: "Login" }), _jsx(Link, { to: '/signup', children: "Sign Up" })] }));
-};
-export default Home;
