925e5637f5d0464e27b24f3d4b8d94e5c3f71cce
Djamal DIB <d.dib@esi-sba.dz>
2025-05-23 11:58:26 +0100

    fix vercel

--- client/src/pages/ProfessorSignUp.tsx
+++ client/src/pages/ProfessorSignUp.tsx
@@ -8,7 +8,7 @@ import { Link, useNavigate } from "react-router-dom"
 import Input from "./components/Input"
 import { useState } from "react"
 import { useTheme } from "../main"
-import { Eye24Filled, EyeOff24Filled, EyeOffRegular, EyeRegular } from "@fluentui/react-icons"
+import { Eye24Filled, EyeOff24Filled} from "@fluentui/react-icons"
 
 const useStyles = makeStyles({
   background: {
