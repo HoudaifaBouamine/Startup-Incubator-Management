ec1352e35ea91c0b423f7590b431ea826281177c
Djamal DIB <d.dib@esi-sba.dz>
2025-05-23 11:07:20 +0100

    fix vercel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [],
-})
+import { defineConfig } from 'vite';
+   import react from '@vitejs/plugin-react';
+
+   export default defineConfig({
+     plugins: [react()],
+   });
